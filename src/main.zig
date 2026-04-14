d8f0cf2c8950c6d56376189f02f3f5f91c7346e1
Sultan Majed <sultan.majed@proton.me>
2026-04-14 14:58:31 +0300

    feat: add random to gen fake nubmers, feat: add mutex to make sure that thread will not make race condation.

--- src/main.zig
+++ src/main.zig
@@ -1,37 +1,120 @@
 const std = @import("std");
-const clap = @import("clap");
+const args_parser = @import("args_parser.zig");
+
+//                            .ssSSSSss.
+//                          .ER'      `AM.
+//                        .ST'          `CS.
+//                       .E'  .S.    .S.  `S.
+//                      .L'   SSS    SSS   `S.
+//                      S'    `S'    `S'    `S
+//                      S                    S
+//                      S                    S
+//                      S.  s.          .s   S
+//                      `S. `"s.      .s"'  S'
+//                       `S.  `"ss..ss"'  .S'
+//                        `SS.    ~~    .SS'
+//                          `SS.      .SS'
+//                            `SSssssSS'
+//
+//  .     .   .      o       .          .       *  . .     .  .    *
+//   .  *  |     .    .            .   .     .   .     * .    . .  
+//       --o--   This project is just advanced stupidity!  *   |     
+//    *    |     .   *  .   .      .    . *     .   .   .    --*--  .
+//   .    .    *    .     .    .       . . .      .        .   |   . 
+
+const print = std.debug.print;
+const exit = std.process.exit;
+const Mutex = std.Io.Mutex;
+
+const CurrentMeasurement = struct {
+    value: isize,
+    Level: enum {
+        Danger,
+        Normal,
+        UnderTheRate,
+    },
+    mutex: Mutex,
+};
 
 pub fn main(init: std.process.Init) !void {
-    // First we specify what parameters our program can take.
-    // We can use `parseParamsComptime` to parse a string into an array of `Param(Help)`.
-    const params = comptime clap.parseParamsComptime(
-        \\-h, --help             Display this help and exit.
-        \\-n, --number <usize>   An option parameter, which takes a value.
-        \\-s, --string <str>...  An option parameter which can be specified multiple times.
-        \\<str>...
-        \\
-    );
+    const io = init.io;
+    const allocator = init.arena.allocator();
 
-    // Initialize our diagnostics, which can be used for reporting useful errors.
-    // This is optional. You can also pass `.{}` to `clap.parse` if you don't
-    // care about the extra information `Diagnostics` provides.
-    var diag = clap.Diagnostic{};
-    var res = clap.parse(clap.Help, &params, clap.parsers.default, init.minimal.args, .{
-        .diagnostic = &diag,
-        .allocator = init.gpa,
-    }) catch |err| {
-        // Report useful error and exit.
-        try diag.reportToFile(init.io, .stderr(), err);
-        return err;
-    };
-    defer res.deinit();
+    var prng: std.Random.DefaultPrng = .init(blk: {
+        var seed: u64 = undefined;
+        // try std.posix.getrandom(std.mem.asBytes(&seed));
+        io.random(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    const rand = prng.random();
 
-    if (res.args.help != 0)
-        std.debug.print("--help\n", .{});
-    if (res.args.number) |n|
-        std.debug.print("--number = {}\n", .{n});
-    for (res.args.string) |s|
-        std.debug.print("--string = {s}\n", .{s});
-    for (res.positionals[0]) |pos|
-        std.debug.print("{s}\n", .{pos});
+    const range = args_parser.parse(init, allocator) catch exit(1);
+
+    print("from = {d}\n", .{range.from});
+    print("to   = {d}\n", .{range.to});
+
+    poll: {
+        var current_measurement = CurrentMeasurement{
+            .value = 0, 
+            .Level = .Normal,
+            .mutex = .init,
+        };
+
+        const income = try 
+            std.Thread.spawn(
+                .{}, 
+                income_stream, 
+                .{io, rand, range, &current_measurement}
+            );
+        defer income.join();
+
+        const check = try 
+            std.Thread.spawn(
+                .{}, 
+                electro_checker, 
+                .{io, range, &current_measurement}
+            );
+        defer check.join();
+
+        if (false) {
+            break :poll;
+        }
+    }
+
+    print("Quit...\n", .{});
+}
+
+/// Here is where the data come from.
+fn income_stream(io: std.Io, rand: std.Random, range: args_parser.ArgsRange, current: *CurrentMeasurement) !void {
+    while (true) {
+        try current.*.mutex.lock(io);
+        defer {
+            current.*.mutex.unlock(io);
+            io.sleep(.fromMilliseconds(650), .awake) catch undefined;
+        }
+
+        const random_number = rand.intRangeAtMost(isize, range.from - 10, range.to + 10);
+        current.*.value = random_number;
+        print("income_stream:   {any}\n", .{random_number});
+    }
+}
+
+fn electro_checker(io: std.Io, range: args_parser.ArgsRange, current: *CurrentMeasurement) !void {
+    while (true) {
+        try current.*.mutex.lock(io);
+        defer {
+            current.*.mutex.unlock(io);
+            io.sleep(.fromMilliseconds(650), .awake) catch undefined;
+        }
+
+        if (current.*.value > range.to) {
+            current.*.Level = .Danger;
+        } else if (current.*.value < range.from) {
+            current.*.Level = .UnderTheRate;
+        } else {
+            current.*.Level = .Normal;
+        }
+
+        print("electro_checker: {any}\n", .{current.Level});
+    }
 }
